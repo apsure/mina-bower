261e3d840e266f38f28467ecd21c18002b560a0e
Nathan Palmer <nathan@nathanpalmer.com>
2015-01-02 16:05:53 -0500

    Added the bower code plus extended Mina::Helpers to get set_default

--- lib/mina/bower.rb
+++ lib/mina/bower.rb
@@ -1,7 +1,19 @@
+require "mina"
 require "mina/bower/version"
+require "rake"
 
-module Mina
-  module Bower
-    # Your code goes here...
+namespace :bower do
+  extend Mina::Helpers
+
+  set_default :bower_bin, 'bin/rake bower:'
+  set_default :bower_options, "RAILS_ENV=\"#{rails_env}\""
+
+  # Installs assets.
+  desc "Install dependencies using Bower."
+  task :install do
+    queue %{
+      echo "-----> Installing dependencies using Bower"
+      #{echo_cmd %[#{bower_bin}install:deployment #{bower_options}]}
+    }
   end
 end
--- mina-bower.gemspec
+++ mina-bower.gemspec
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.6"
   spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "mina", "~> 0.3"
 end
